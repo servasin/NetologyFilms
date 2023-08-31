73fd9bdfeab1377d86589b0afa6bf590c77d65ae
Sergei Vasin <vvs221@bk.ru>
2023-08-31 23:20:44 +0300

    change function Stars

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -7,19 +7,19 @@ type rangeStars =  {
 function Stars({count}:rangeStars) {  
   if (count<1 || count>5) {
     return null
-  } else {
-    const arr = new Array(count).fill('')
+  }
+    const arr = new Array(count).fill(<Star />)
     
     return (
       <ul className="stars-list">
-        {arr.map((_, i) =>
+        {arr.map((el, i) =>
           <li key={i}>
-            <Star />
+            {el}
           </li>
         )}  
       </ul>
     )
   }
-}
+
 
 export default Stars
